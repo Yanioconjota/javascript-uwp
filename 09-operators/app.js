9947902762209578981fc8d3890f0c79efb16d7f
Janio Isacura <janioisacura@gmail.com>
2021-11-24 16:39:15 -0300

    Operator examples update

--- 09-operators/app.js
+++ 09-operators/app.js
@@ -14,3 +14,32 @@ function isGreater(a, b) {
 
 isGreater(3,4);
 //false
+
+function calculationExample(a, b, c) {
+	console.log(a + b * c);
+}
+
+calculationExample(3, 4, 5);
+//23
+
+function calculationExample2(a, b, c) {
+	console.log(a * b * c);
+}
+
+calculationExample2(3, 4, 5);
+//60 --> 3 * 4 = 12 --> 12 * 5
+
+var a = 2, b = 3, c = 4;
+a = b = c;
+console.log(a); //4
+console.log(b); //4
+console.log(c); //4
+
+var d = (3 + 4) * 5;
+console.log(d);
+//35
+
+var e = 1 + '2'
+console.log(e);
+//12
+
