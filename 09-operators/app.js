8a02316c348e7ce905266e471a0d82c547113317
Janio Isacura <janioisacura@gmail.com>
2021-11-23 22:49:35 -0300

    New example added

---
+++ 09-operators/app.js
@@ -0,0 +1,16 @@
+var a = 3 + 4;
+
+function add(a, b) {
+	//return a + b;
+	console.log(a + b)
+}
+
+add(3,4);
+//7
+
+function isGreater(a, b) {
+	console.log(a > b);
+}
+
+isGreater(3,4);
+//false
