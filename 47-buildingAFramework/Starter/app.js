520487a92f725768e39e59eb606c0c10a0fd7928
Janio Isacura <janioisacura@gmail.com>
2022-02-15 16:57:38 -0300

    New example added

--- 47-buildingAFramework/Starter/app.js
+++ 47-buildingAFramework/Starter/app.js
@@ -3,3 +3,5 @@ var g = G$('Homero', 'Thompson');
 //2 - invoco el método saludar encadenándole distintas ejecuciiones
 g.greet().setLang('es').greet(true);//Hello Homero!
 //g.setLang('gr').greet();//Uncaught Invalid language
+
+g.greet().setLang('en').log();//Logged in: Homero Thompson
